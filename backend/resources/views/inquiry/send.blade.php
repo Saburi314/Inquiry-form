c465a8bbd3901975af26604379337834410438ab
Saburi314 <y.saburi@prosysta.co.jp>
2022-09-22 21:45:13 +0900

    first time to master

---
+++ backend/resources/views/inquiry/send.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('title', '完了画面')
+
+@section('content')
+<h1>送信完了しました。</h1>
+<p>お問い合わせありがとうございました。</p>
+@endsection
---
+++ backend/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InquiryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//入力ページ
+Route::get('/inquiry', [InquiryController::class, 'index'])->name('inquiry.index');
+
+//確認ページ
+Route::post('/inquiry/confirm', [InquiryController::class, 'confirm'])->name('inquiry.confirm');
+
+//送信完了ページ
+Route::post('/inquiry/send', [InquiryController::class, 'send'])->name('inquiry.send');
+
